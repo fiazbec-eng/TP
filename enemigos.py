12ff7d724f2306c25fdbfd335a82be1186e654b7
nickk <ifalcone@alumno.huergo.edu.ar>
2026-09-02 11:47:27 -0300

    Update enemigos.py

--- enemigos.py
+++ enemigos.py
@@ -1,7 +1,30 @@
-import pygame
-from jugadores import vida_1, vida_2
-enemigos = {"Orco Rojo": 
+import pygame, random
+#from jugadores import vida_1, vida_2
 
-}
+class Enemigo():
+    def __init__(self, nombre, vida):
+        self.nombre = nombre
+        self.vida = vida
+
+class OrcoRojo(Enemigo):
+    def __init__(self, nombre, vida):
+        self.nombre 
+
+class esqueletoNegro(Enemigo):
+    def __init__(self):
+        super().__init__()
+
+orco = OrcoRojo("Orco Rojo", 100)
+en = esqueletoNegro()
+
+enemigos = [orco, en]
+enemigo = random.choice(enemigos)
+
+print("Te ataca", enemigo.nombre)
+
+
+
+
 def enemigos():
     while vida_1 != 0:
+        pass
