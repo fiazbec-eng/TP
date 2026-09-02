10b0a473582f23d652a262fe364cae9d32dd6523
nickk <ifalcone@alumno.huergo.edu.ar>
2026-09-02 11:33:28 -0300

    .

--- enemigos.py
+++ enemigos.py
@@ -1,4 +1,7 @@
 import pygame
 from jugadores import vida_1, vida_2
+enemigos = {"Orco Rojo": 
+
+}
 def enemigos():
     while vida_1 != 0:
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame
-import random
 import sys
 
 pygame.init()
@@ -37,141 +36,12 @@ espacio = 14
 
 inicio_x = (ANCHO - boton_ancho) // 2
 inicio_y = 255
-particulas = []
-
-for i in range(28):
-    particulas.append({
-        "x": random.randint(0, ANCHO),
-        "y": random.randint(ALTO, ALTO + 300),
-        "velocidad": random.uniform(0.5, 1.5),
-        "tamaño": random.randint(2, 4),
-        "desplazamiento": random.uniform(-0.5, 0.5)
-    })
-
 
 def dibujar_fondo():
     ventana.fill(NEGRO)
-
-    for radio in range(350, 0, -10):
-        intensidad = int(50 * (1 - radio / 350))
-        superficie = pygame.Surface((ANCHO, ALTO), pygame.SRCALPHA)
-
-        pygame.draw.ellipse(
-            superficie,
-            (140, 13, 13, intensidad),
-            (
-                ANCHO // 2 - radio,
-                ALTO - radio // 2,
-                radio * 2,
-                radio
-            )
-        )
-
-        ventana.blit(superficie, (0, 0))
-
-
-def dibujar_particulas():
-    for p in particulas:
-        pygame.draw.circle(
-            ventana,
-            ROJO,
-            (int(p["x"]), int(p["y"])),
-            p["tamaño"]
-        )
-
-        p["y"] -= p["velocidad"]
-        p["x"] += p["desplazamiento"]
-
-        if p["y"] < -10:
-            p["y"] = ALTO + random.randint(10, 100)
-            p["x"] = random.randint(0, ANCHO)
-
-
-def dibujar_sello():
-    centro_x = ANCHO // 2
-    centro_y = 105
-
-    pygame.draw.circle(
-        ventana,
-        ROJO,
-        (centro_x, centro_y),
-        46,
-        2
-    )
-
-    pygame.draw.line(
-        ventana,
-        ROJO_MEDIO,
-        (centro_x, centro_y - 42),
-        (centro_x, centro_y + 42),
-        1
-    )
-
-    pygame.draw.line(
-        ventana,
-        ROJO_MEDIO,
-        (centro_x - 42, centro_y),
-        (centro_x + 42, centro_y),
-        1
-    )
-
-    pygame.draw.line(
-        ventana,
-        ROJO_MEDIO,
-        (centro_x - 30, centro_y - 30),
-        (centro_x + 30, centro_y + 30),
-        1
-    )
-
-    pygame.draw.line(
-        ventana,
-        ROJO_MEDIO,
-        (centro_x + 30, centro_y - 30),
-        (centro_x - 30, centro_y + 30),
-        1
-    )
-
-    puntos = [
-        (centro_x, centro_y - 30),
-        (centro_x + 20, centro_y + 15),
-        (centro_x - 23, centro_y - 13),
-        (centro_x + 23, centro_y - 13),
-        (centro_x - 20, centro_y + 15)
-    ]
-
-    pygame.draw.polygon(
-        ventana,
-        ROJO,
-        puntos,
-        1
-    )
-
-
-def dibujar_texto():
-    titulo = fuente_titulo.render(
-        "REQUIEM",
-        True,
-        HUESO
-    )
-
-    titulo_rect = titulo.get_rect(
-        center=(ANCHO // 2, 175)
-    )
-
-    ventana.blit(titulo, titulo_rect)
-
-    subtitulo = fuente_subtitulo.render(
-        "el infierno espera tu mando",
-        True,
-        HUESO_OSCURO
-    )
-
-    subtitulo_rect = subtitulo.get_rect(
-        center=(ANCHO // 2, 215)
-    )
-
-    ventana.blit(subtitulo, subtitulo_rect)
-
+    fondo_menu = pygame.image.load("menu.jfif").convert()
+    fondo = pygame.transform.scale(fondo, ANCHO , ALTO)
+    
 
 def dibujar_botones(mouse_pos):
     for i, texto in enumerate(botones):
@@ -285,9 +155,6 @@ while ejecutando:
                             print("Reiniciando juego...")
 
     dibujar_fondo()
-    dibujar_particulas()
-    dibujar_sello()
-    dibujar_texto()
     dibujar_botones(mouse_pos)
 
     pygame.display.flip()
