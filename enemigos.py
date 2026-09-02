c373752fa7ee019285adc54eba7e0202dbc9448d
nickk <ifalcone@alumno.huergo.edu.ar>
2026-09-02 10:29:32 -0300

    .

---
+++ enemigos.py
@@ -0,0 +1,4 @@
+import pygame
+from jugadores import vida_1, vida_2
+def enemigos():
+    while vida_1 != 0:
---
+++ jugadores.py
@@ -0,0 +1,7 @@
+import pygame
+
+def jugador_1():
+    vida_1 = 100
+    while vida_1 != 0:
+
+def jugador_2():
