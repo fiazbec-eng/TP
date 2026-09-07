23356d271d7f5141e595d126d4434e471a9a77ad
mechikuroki <kurokimercedes@gmail.com>
2026-09-07 08:45:28 -0300

    .

---
+++ enemigos.py
@@ -0,0 +1,29 @@
+import pygame, random
+
+
+class Enemigo():
+    def __init__(self, nombre, vida):
+        self.nombre = nombre
+        self.vida = vida
+
+
+class OrcoRojo(Enemigo):
+    def __init__(self):
+        super().__init__("Orco Rojo", 100)
+
+
+class esqueletoNegro(Enemigo):
+    def __init__(self):
+        super().__init__("Esqueleto Negro", 100)
+
+
+orco = OrcoRojo()
+esqueleto_negro= esqueletoNegro()
+
+enemigos = [OrcoRojo, esqueletoNegro]
+
+def crear_enemigos(x):
+    enemigo = random.choice(enemigos)
+    while vida:
+        pass
+crear_enemigos(enemigo)
